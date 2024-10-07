8161d8b4fefa4f590435d2395039d2922984f750
Methsavi <135610100+Methsavi@users.noreply.github.com>
2024-10-07 22:23:28 +0530

    create files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="">"Good luck! Believe in yourself and your abilities—you’ve got this! Stay focused, stay positive, and keep moving forward. Success is just around the corner!"</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
