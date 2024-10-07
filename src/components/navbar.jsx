b0b64b0e60e61660dcb1419d5aff5f06ce2e6bbb
Methsavi <135610100+Methsavi@users.noreply.github.com>
2024-10-07 22:45:24 +0530

    did some changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const navbar = () => {
   return (
     <>
-    <div className="bg-blue-500 text-white p-4">
+    <div>
+      <h1 className='flex flex-col items-center justify-center mt-4 text-center mb-4 text-xl'>Welcome isu & navo. Let's make our 1st webpage together</h1>
+    </div>
+    <div className="bg-blue-500 text-white p-4 flex flex-col items-center justify-center">
       <h1 className="">"Good luck! Believe in yourself and your abilities—you’ve got this! Stay focused, stay positive, and keep moving forward. Success is just around the corner!"</h1>
     </div>
     </>
