d8dd569a8317d9ba7909b0bc4e2f61b8944c813e
Methsavi <135610100+Methsavi@users.noreply.github.com>
2024-10-07 22:30:47 +0530

    did some changes

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <>
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="">"Good luck! Believe in yourself and your abilities—you’ve got this! Stay focused, stay positive, and keep moving forward. Success is just around the corner!"</h1>
+    </div>
+    </>
+  )
+}
+
+export default navbar
